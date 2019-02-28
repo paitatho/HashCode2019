b790e09ad86a9a8dad15e5a9e28ef1b0ae3fbd71
paitatho <tom.paita@hotmail.fr>
2019-03-01 00:02:05 +0100

    opti

--- main.py
+++ main.py
@@ -5,18 +5,29 @@ from qpe import *
 
 def main():
     #retourne le tableau bien formaté
-    dataset, indice = lectureFichier("a_example.txt")
-
+    dataset, indice = lectureFichier("c_memorable_moments.txt")
+    print("lecture OK\n")
+    #print(len(indice))
     #Calcul la matrice des tags communs
+    """
     m_commonTags = countCommonTags(dataset)
+    print("comptage tags en commun OK\n")
     #Calcul le facteur d'intérêt
     m_interestFactor = interestFactor(dataset, m_commonTags)
+    print("calcul matrice facteur OK\n")
+    """
+    m_interestFactor = computeMatrix(dataset)
+    print("calcul matrice facteur OK\n")
+
     #retourne le ranking  
     slides = outputConstruction(m_interestFactor)
+    print("calcul suite slides OK\n")
+    #print(slides)
     #slides = [0,2,1]
     
     #écrit les résultats
-    ecritureFichier("a_example_result.txt",slides,indice)
+    ecritureFichier("c_memorable_moments_result.txt",slides,indice)
+    print("ecriture OK\n")
 
 
 main()
--- qpe.py
+++ qpe.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
+def computeMatrix(dataset):
+    n = len(dataset)
+    matrice = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(i, n):
+            x = dataset[i]
+            y = dataset[j]
+            res = len(x[1:]) + len(y[1:]) - len(set(x[1:] + y[1:]))
+            matrice[i][j] = min(res, min(int(dataset[i][0]) - res , int(dataset[j][0]) - res))
+            matrice[j][i] = matrice[i][j]
+    return matrice
+
+"""
 def countCommonTags(dataset):
     matrice = [[0]*len(dataset) for _ in range(len(dataset))]
     for i,x in enumerate(dataset):
@@ -16,26 +29,39 @@ def interestFactor(dataset, m_commonTags):
             matrice[j][i] = res
     return matrice
 
+
+def computeMatrix(dataset):
+    m_commonTags = countCommonTags(dataset)
+    m_interestFactor = interestFactor(dataset, m_commonTags)
+    return m_interestFactor
+"""
+
+
 def outputConstruction(m_interestFactor):
     # Initialisation
     max_factor = 0
     l,c = 0,0
     rank = []
+    
     for i in range(len(m_interestFactor)):
         for j in range(i,len(m_interestFactor)):
             if max_factor < m_interestFactor[i][j]:
                 max_factor = m_interestFactor[i][j]
                 l,c = i,j
+    not_attributed = list(range(len(m_interestFactor)))
     rank.append(l)
+    not_attributed.remove(l)
     rank.append(c)
-    max_factor = 0
+    not_attributed.remove(c)    
     while len(rank) != len(m_interestFactor):
         c = rank[-1]
-        for i in range(len(m_interestFactor)):
-            if i not in rank and max_factor < m_interestFactor[i][c]:
-                max_factor = 0
+        max_factor = 0
+        for i in not_attributed:
+             if max_factor <= m_interestFactor[c][i]:
+                max_factor = m_interestFactor[c][i]
                 l = i
         rank.append(l)                    
+        not_attributed.remove(l)
     return rank
     
 
